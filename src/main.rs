fb2b4e63a829fae39cdf4ddddecfcb7738449d14
1ET <1374052652@qq.com>
2024-07-15 16:32:59 +0800

    support generic csv to json and yaml output

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use clap::Parser;
 use rcli::{process_csv, Opts, SubCommand};
 
-/*
-* rcli csv -i input.csv -o output.json --header -d ','
-*/
-
 fn main() -> anyhow::Result<()> {
     let opts: Opts = Opts::parse();
     match opts.cmd {
-        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+        SubCommand::Csv(opts) => {
+            let output = if let Some(output) = opts.output {
+                output.clone()
+            } else {
+                format!("output.{}", opts.format)
+            };
+            process_csv(&opts.input, output, opts.format)?;
+        }
     }
     Ok(())
 }
